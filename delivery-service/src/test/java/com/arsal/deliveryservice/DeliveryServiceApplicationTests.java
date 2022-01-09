fe25a7bd6c6159c557dd47a73b7fa2acf2fd4970
arsaljamal <arsaljamal2348@gmail.com>
2022-01-09 17:53:33 +0500

    initial code commit for delivery-service

---
+++ delivery-service/src/test/java/com/arsal/deliveryservice/DeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arsal.deliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
