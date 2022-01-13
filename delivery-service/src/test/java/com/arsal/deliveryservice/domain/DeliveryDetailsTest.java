402dcf9ec976142539c6b7a290a489b1f582da24
arsaljamal <arsaljamal2348@gmail.com>
2022-01-13 17:20:15 +0500

    added domain test case for delivery-service and ticketing-service

--- delivery-service/src/test/java/com/arsal/deliveryservice/domain/DeliveryDetailsTest.java
+++ delivery-service/src/test/java/com/arsal/deliveryservice/domain/DeliveryDetailsTest.java
@@ -1,4 +1,43 @@
 package com.arsal.deliveryservice.domain;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
 public class DeliveryDetailsTest {
+
+    private final Date nowTimeStamp = new Date();
+
+    public DeliveryDetails setUp() {
+        DeliveryDetails deliveryDetails = new DeliveryDetails();
+        deliveryDetails.setProcessed(Boolean.TRUE);
+        deliveryDetails.setDeliveryStatus(DeliveryStatus.Order_Delivered);
+        deliveryDetails.setCustomerType(CustomerType.VIP);
+        deliveryDetails.setRiderRating(RiderRating.FOUR);
+        deliveryDetails.setCurrentDistanceFromDestinationInMeters(10);
+        deliveryDetails.setRestaurantMeanTimeToPrepareFoodInMinutes(10);
+        deliveryDetails.setTimeToReachDestination(nowTimeStamp);
+        deliveryDetails.setExpectedDeliveryTime(nowTimeStamp);
+        deliveryDetails.setEstimatedTimeOfDelivery(nowTimeStamp);
+        return deliveryDetails;
+    }
+
+
+    @Test
+    public void testDeliveryDetailsGetters() {
+        DeliveryDetails deliveryDetailsTest = setUp();
+        assertEquals(deliveryDetailsTest.getProcessed(), Boolean.TRUE);
+        assertEquals(deliveryDetailsTest.getDeliveryStatus(), DeliveryStatus.Order_Delivered);
+        assertEquals(deliveryDetailsTest.getCustomerType(), CustomerType.VIP);
+        assertEquals(deliveryDetailsTest.getRiderRating(), RiderRating.FOUR);
+        assertEquals(10, (int) deliveryDetailsTest.getCurrentDistanceFromDestinationInMeters());
+        assertEquals(10, (int) deliveryDetailsTest.getRestaurantMeanTimeToPrepareFoodInMinutes());
+        assertEquals(deliveryDetailsTest.getTimeToReachDestination(), nowTimeStamp);
+        assertEquals(deliveryDetailsTest.getExpectedDeliveryTime(), nowTimeStamp);
+        assertEquals(deliveryDetailsTest.getEstimatedTimeOfDelivery(), nowTimeStamp);
+    }
+
+
 }
--- ticketing-service/src/test/java/com/arsal/ticketingservice/domain/TicketsTest.java
+++ ticketing-service/src/test/java/com/arsal/ticketingservice/domain/TicketsTest.java
@@ -1,4 +1,41 @@
 package com.arsal.ticketingservice.domain;
 
+
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
 public class TicketsTest {
+
+    private final Date nowTimeStamp = new Date();
+    private static final String Order_Delivered = "Order_Delivered";
+    private static final String VIP = "VIP";
+    private static final String FOUR = "FOUR";
+
+    public Tickets setUp() {
+        Tickets tickets = new Tickets();
+        tickets.setDeliveryId(1L);
+        tickets.setDeliveryStatus(Order_Delivered);
+        tickets.setCustomerType(VIP);
+        tickets.setRiderRating(FOUR);
+        tickets.setTimeToReachDestination(nowTimeStamp);
+        tickets.setExpectedDeliveryTime(nowTimeStamp);
+        tickets.setEstimatedTimeOfDelivery(nowTimeStamp);
+
+        return tickets;
+    }
+
+    @Test
+    public void testTicketsGetters() {
+        Tickets ticketsTest = setUp();
+        assertEquals(ticketsTest.getDeliveryId(), Long.valueOf(1L));
+        assertEquals(ticketsTest.getDeliveryStatus(), Order_Delivered);
+        assertEquals(ticketsTest.getCustomerType(), VIP);
+        assertEquals(ticketsTest.getRiderRating(), FOUR);
+        assertEquals(ticketsTest.getTimeToReachDestination(), nowTimeStamp);
+        assertEquals(ticketsTest.getExpectedDeliveryTime(), nowTimeStamp);
+        assertEquals(ticketsTest.getEstimatedTimeOfDelivery(), nowTimeStamp);
+    }
 }
